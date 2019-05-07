d6d455e5d0c19ea376f71df1e57c16ad1bf5cbca
Joonhak <ofnullable@gmail.com>
2019-05-08 02:22:11 +0900

    Create reducers

---
+++ front/reducers/post.js
@@ -0,0 +1,47 @@
+export const initialState = {
+    mainPosts: [{
+        User: {
+            id: 1,
+            nickname: 'joonak',
+        },
+        content: '첫번째 게시글',
+        img: 'http://img.worksout.co.kr/upload/image/editor/20190430/201904301054390291.jpg',
+        createdAt: Date.now(),
+    }],
+};
+
+export const ADD_POST = 'ADD_POST';
+export const ADD_DUMMY = 'ADD_DUMMY';
+
+export const addPost = {
+    type: ADD_POST,
+    data: {},
+};
+
+export const addDummy = {
+    type: ADD_DUMMY,
+    data: {
+        content: 'Hi',
+        UserId: 1,
+        User: {
+            nickname: 'joonak',
+        },
+    },
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_POST:
+            return {
+                ...state,
+                mainPosts: [action.data, ...state.mainPosts],
+            };
+        case ADD_DUMMY:
+            return {
+                ...state,
+                mainPosts: [action.data, ...state.mainPosts],
+            };
+        default:
+            return state;
+    }
+};
